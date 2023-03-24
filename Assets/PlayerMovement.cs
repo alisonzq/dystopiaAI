4d5c2377b3c43fcd73f24390a38c3f51855ac886
Tristan Cholette <tristancholette@gmail.com>
2023-03-24 09:17:03 -0400

    Player X movement and Z Rotate fix

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -4,17 +4,22 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start() {
+    private Rigidbody2D rb;
 
+    // Start is called before the first frame update
+    private void Start() {
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
-    void Update() {
-        if (Input.GetKey("space") & GetComponent<Rigidbody2D>().velocity.y <= 0.01 & GetComponent<Rigidbody2D>().velocity.y >= -0.01) {
-            GetComponent<Rigidbody2D>().velocity = new Vector3(0, 7, 0);
+    private void Update() {
+        float dirX = Input.GetAxisRaw("Horizontal");
+
+        rb.velocity = new Vector2(dirX * 7f, rb.velocity.y);
+
+        if (Input.GetButton("Jump") & GetComponent<Rigidbody2D>().velocity.y <= 0.01 & GetComponent<Rigidbody2D>().velocity.y >= -0.01) {
+            rb.velocity = new Vector3(rb.velocity.x, 14f);
         }
-        float b = GetComponent<Rigidbody2D>().velocity.y;
-        Debug.Log(b);
+     
     }
 }
