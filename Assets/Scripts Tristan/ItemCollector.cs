8da395bb6cccee37ee7d0799053ca8628f587f71
Tristan Cholette <tristancholette@gmail.com>
2023-03-30 01:02:04 -0400

    Bananas!
    Banana pickup with banana count and banana despawn animation

---
+++ Assets/Scripts Tristan/ItemCollector.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ItemCollector : MonoBehaviour
+{
+
+    private int bananas = 0;
+
+    [SerializeField] private Text bananasText;
+
+
+    private void OnTriggerEnter2D(Collider2D collision) {
+        if (collision.gameObject.CompareTag("Banana")) {
+
+            bananas++;
+            bananasText.text = "" + bananas;
+
+            collision.gameObject.GetComponent<Animator>().SetBool("picked", true);
+            Destroy(collision.gameObject.GetComponent<BoxCollider2D>());
+
+        } else collision.gameObject.GetComponent<Animator>().SetBool("picked", false);
+    }
+
+}
